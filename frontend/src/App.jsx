dbce8d6cbf7bfcf8c04991ae512294ecaffa9e89
Adityasinghbhadoriya <aditya0303singh@gmail.com>
2026-04-21 22:37:42 +0530

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
+import Home from "./Pages/Home"
 // import Restaurants from "./Pages/Restaurant"
 import Clubs from "./Pages/Clubs"
 import Experiences from "./Pages/Experiences"
